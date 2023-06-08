1aad3acd97890e3837acd9e5d5cc7548830a1f5f
MasterAtharvaJ <me.atharvajadhav@gmail.com>
2023-06-08 09:08:12 +0530

    commit2

--- app.py
+++ app.py
@@ -4,22 +4,17 @@ import openai
 
 app = Flask(__name__)
 
-# Set up OpenAI API credentials
 openai.api_key = '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'
 
 
-# Define the default route to return the index.html file
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-# Define the /api route to handle POST requests
 @app.route("/api", methods=["POST"])
 def api():
-    # Get the message from the POST request
     message = request.json.get("message")
-    # Send the message to OpenAI's API and receive the response
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
